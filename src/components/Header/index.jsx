fbff6091f1e773aba519fdd47381471b5d48383a
Olufayo Jeremiah <olufayojeremiah@gmail.com>
2019-05-04 18:36:29 +0100

    install formik to handle forms

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './header.scss';
 
 export const Header = () => (
-  <div className=''>
+  <div className='header'>
     <h1>Random Phone Number Generator</h1>
   </div>
 )
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,10 +1,14 @@
 import React, { Fragment } from 'react';
 
+import NumberGenerator from '../NumberGenerator';
 import { Header } from '../Header';
+
+import './home.scss';
 
 const Home = () => (
   <Fragment>
     <Header />
+    <NumberGenerator />
   </Fragment>
 );
 
